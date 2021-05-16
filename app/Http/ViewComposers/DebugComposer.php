481dd4bb2cebd183d56cc7bd3d0017551e075201
Kazuki Yamashiro <yamashiro.k@impl.co.jp>
2021-05-16 21:15:12 +0900

    https://crieit.net/posts/Laravel-debugbar

---
+++ app/Http/ViewComposers/DebugComposer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\ViewComposers;
+
+use Illuminate\Contracts\View\View;
+
+class DebugComposer
+{
+    /**
+     *
+     * @param View $view
+     * @return void
+     */
+    public function compose(View $view)
+    {
+        \Debugbar::info($view->getData());
+    }
+}
